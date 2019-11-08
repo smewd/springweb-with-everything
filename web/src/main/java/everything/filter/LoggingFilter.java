089bc36e4805387790823bc9a396c9dd70df5ae2
Johan Sandberg <johan@smood.com>
2019-11-08 16:36:17 +0100

    annotating filters to autodetect instead of initialize them manually

--- web/src/main/java/everything/filter/LoggingFilter.java
+++ web/src/main/java/everything/filter/LoggingFilter.java
@@ -7,17 +7,22 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import static javax.servlet.DispatcherType.REQUEST;
 
 
+
+@WebFilter(dispatcherTypes = REQUEST, urlPatterns = "*")
 public class LoggingFilter implements Filter
 {
 	private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
 
 
 
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
 			throws IOException, ServletException
 	{
